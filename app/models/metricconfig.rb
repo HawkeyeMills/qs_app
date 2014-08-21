408a65ce5d2b3560bb4e3da0bed1d71136d382a1
HawkeyeMills <brandon@blendcard.com>
2014-08-21 01:03:17 -0500

    deleting files from Git

--- app/models/metricconfig.rb
+++
@@ -1,7 +0,0 @@
-class Metricconfig < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :metric
-  default_scope -> { order('orderby DESC') }
-  validates :value, presence: true, length: { maximum: 25 }
-  validates :user_id, presence: true
-end
