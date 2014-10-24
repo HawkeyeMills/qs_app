4dbdb209c5f984bd854c598a558ef0de64f7bd1a
HawkeyeMills <brandon@blendcard.com>
2014-10-24 15:57:46 -0500

    troublshooting prod differences

--- app/models/metric_grade.rb
+++ app/models/metric_grade.rb
@@ -3,17 +3,17 @@ class MetricGrade < ActiveRecord::Base
 	has_many :metrics
 
 	def MetricGrade.calcStandard(metricConfigID, metricId, gcid, metrics)
-		#logger.info (" calcStandard!!!!" )
+		logger.info (" calcStandard!!!!" )
 		@grade_configs = GradeConfig.all
 		# get grade_config.goal based on metricConfigID
 		gc_row = @grade_configs.find_by metric_config_id: metricConfigID
 		gc_goal = gc_row.goal
-		#logger.info("gc_goal = #{gc_goal}")
+		logger.info("gc_goal = #{gc_goal}")
 		gc_weight = gc_row.weight
-		#logger.info("gc_weight = #{gc_weight}")
+		logger.info("gc_weight = #{gc_weight}")
 		# get metric.value m_value from metric.id
 		m_value = metrics.find(metricId).value
-		#logger.info ("m_value = #{m_value}")
+		logger.info ("m_value = #{m_value}")
 
 		# initialize mg_points (metric grade points = outPoints)
 		mg_points = 0
@@ -21,22 +21,22 @@ class MetricGrade < ActiveRecord::Base
 		# do biz logic
 		if m_value.to_i == ''
 			mg_points = mg_points
-			#logger.info("1 POINTS = #{mg_points}")
+			logger.info("1 POINTS = #{mg_points}")
 		elsif m_value >= gc_goal.to_i
 			mg_points = gc_weight
-			#logger.info("2 POINTS = #{mg_points}")
+			logger.info("2 POINTS = #{mg_points}")
 		elsif m_value < gc_goal.to_i
 			mg_points = (m_value.to_f/gc_goal.to_f)*gc_weight.to_f
-			#logger.info("3 POINTS = #{m_value.to_f}")
-			#logger.info("3 POINTS = #{gc_goal.to_f}")
-			#logger.info("3 POINTS = #{gc_weight.to_f}")
-			#logger.info("3 POINTS = #{mg_points}")
+			logger.info("3 POINTS = #{m_value.to_f}")
+			logger.info("3 POINTS = #{gc_goal.to_f}")
+			logger.info("3 POINTS = #{gc_weight.to_f}")
+			logger.info("3 POINTS = #{mg_points}")
 		end
 
 		# get metric_grade.percentage
 		mg_percentvalue = mg_points / gc_weight
 		mg_percentage = mg_percentvalue * 100
-		#logger.info("mg_percentage = #{mg_percentage}")
+		logger.info("mg_percentage = #{mg_percentage}")
 
 		g_gradeid = nil
 
@@ -44,12 +44,12 @@ class MetricGrade < ActiveRecord::Base
 		grades = Grade.all
 		grades.each do |grade|
 			grademin = grade.minrange
-			#logger.info("grademin = #{grademin}")
+			logger.info("grademin = #{grademin}")
 			grademax = grade.maxrange
-			#logger.info("grademax = #{grademax}")
+			logger.info("grademax = #{grademax}")
 			if mg_percentage.to_f <= grademax.to_f and mg_percentage.to_f > grademin.to_f
 				gradeval = grade.gradevalue
-				#logger.info("GRADE = #{gradeval}")
+				logger.info("GRADE = #{gradeval}")
 				g_gradeid = grade.id
 			elsif mg_percentage.to_f == 0
 				gradeval = "F"
@@ -65,25 +65,25 @@ class MetricGrade < ActiveRecord::Base
 	end
 
 	def MetricGrade.calcWeight(metricConfigID, metricId, gcid, metrics, user)
-		#logger.info (" calcWeight!!!!" )
+		logger.info (" calcWeight!!!!" )
 		@grade_configs = GradeConfig.all
 		# get grade_config.goal based on metricConfigID
 		gc_row = @grade_configs.find_by metric_config_id: metricConfigID
 		gc_goal = gc_row.goal
-		#logger.info("gc_goal = #{gc_goal}")
+		logger.info("gc_goal = #{gc_goal}")
 		gc_weight = gc_row.weight
-		#logger.info("gc_weight = #{gc_weight}")
+		logger.info("gc_weight = #{gc_weight}")
 		# get metric.value m_value from metric.id
 		m_value = metrics.find(metricId).value
-		#logger.info ("m_value = #{m_value}")
+		logger.info ("m_value = #{m_value}")
 		m_date = metrics.find(metricId).metricdate
-		#logger.info ("m_date = #{m_date}")
+		logger.info ("m_date = #{m_date}")
 		m_yesterday = m_date.to_date - 1
-		#logger.info ("m_yesterday = #{m_yesterday}")
+		logger.info ("m_yesterday = #{m_yesterday}")
 		m_yesterdayObj = Metric.where(metric_config_id: metricConfigID, metricdate: m_yesterday).first
-		#logger.info ("m_yesterdayObj = #{m_yesterdayObj}")
+		logger.info ("m_yesterdayObj = #{m_yesterdayObj}")
 		m_yesterdayValue = m_yesterdayObj.value
-		#logger.info ("m_yesterdayValue = #{m_yesterdayValue}")
+		logger.info ("m_yesterdayValue = #{m_yesterdayValue}")
 
 		# initialize mg_points (metric grade points = outPoints)
 		mg_points = 0
@@ -91,19 +91,19 @@ class MetricGrade < ActiveRecord::Base
 		# do biz logic
 		if m_value >= m_yesterdayValue
 			mg_points = mg_points
-			#logger.info("1 POINTS = #{mg_points}")
+			logger.info("1 POINTS = #{mg_points}")
 		elsif m_value < m_yesterdayValue
 			mg_points = gc_weight
-			#logger.info("2 POINTS = #{mg_points}")
+			logger.info("2 POINTS = #{mg_points}")
 		else
 			mg_points = 0
-			#logger.info("NEVER HERE = #{m_value.to_f}")
+			logger.info("NEVER HERE = #{m_value.to_f}")
 		end
 
 		# get metric_grade.percentage
 		mg_percentvalue = mg_points / gc_weight
 		mg_percentage = mg_percentvalue * 100
-		#logger.info("mg_percentage = #{mg_percentage}")
+		logger.info("mg_percentage = #{mg_percentage}")
 
 		g_gradeid = nil
 
@@ -111,12 +111,12 @@ class MetricGrade < ActiveRecord::Base
 		grades = Grade.all
 		grades.each do |grade|
 			grademin = grade.minrange
-			#logger.info("grademin = #{grademin}")
+			logger.info("grademin = #{grademin}")
 			grademax = grade.maxrange
-			#logger.info("grademax = #{grademax}")
+			logger.info("grademax = #{grademax}")
 			if mg_percentage.to_f <= grademax.to_f and mg_percentage.to_f > grademin.to_f
 				gradeval = grade.gradevalue
-				#logger.info("GRADE = #{gradeval}")
+				logger.info("GRADE = #{gradeval}")
 				g_gradeid = grade.id
 			elsif mg_percentage.to_f == 0
 				gradeval = "F"
@@ -135,17 +135,17 @@ class MetricGrade < ActiveRecord::Base
 		#used primarily for workout duration
 		#goal in grade_configs stored like 00:30:00
 		#value in metrics stored like - NEED TO GET WORKOUT DURATION ADDED
-		#logger.info (" calcStandard!!!!" )
+		logger.info (" calcStandard!!!!" )
 		@grade_configs = GradeConfig.all
 		# get grade_config.goal based on metricConfigID
 		gc_row = @grade_configs.find_by metric_config_id: metricConfigID
 		gc_goal = gc_row.goal
-		#logger.info("gc_goal = #{gc_goal}")
+		logger.info("gc_goal = #{gc_goal}")
 		gc_weight = gc_row.weight
-		#logger.info("gc_weight = #{gc_weight}")
+		logger.info("gc_weight = #{gc_weight}")
 		# get metric.value m_value from metric.id
 		m_value = metrics.find(metricId).value
-		#logger.info ("m_value = #{m_value}")
+		logger.info ("m_value = #{m_value}")
 
 		# initialize mg_points (metric grade points = outPoints)
 		mg_points = gc_weight
@@ -153,22 +153,22 @@ class MetricGrade < ActiveRecord::Base
 		# do biz logic
 		if m_value.to_i == ''
 			mg_points = mg_points
-			#logger.info("1 POINTS = #{mg_points}")
+			logger.info("1 POINTS = #{mg_points}")
 		elsif m_value <= gc_goal.to_i
 			mg_points = gc_weight
-			#logger.info("2 POINTS = #{mg_points}")
+			logger.info("2 POINTS = #{mg_points}")
 		else
 			mg_points = (gc_goal.to_f/m_value.to_f)*gc_weight.to_f
-			#logger.info("3 POINTS = #{m_value.to_f}")
-			#logger.info("3 POINTS = #{gc_goal.to_f}")
-			#logger.info("3 POINTS = #{gc_weight.to_f}")
-			#logger.info("3 POINTS = #{mg_points}")
+			logger.info("3 POINTS = #{m_value.to_f}")
+			logger.info("3 POINTS = #{gc_goal.to_f}")
+			logger.info("3 POINTS = #{gc_weight.to_f}")
+			logger.info("3 POINTS = #{mg_points}")
 		end
 
 		# get metric_grade.percentage
 		mg_percentvalue = mg_points / gc_weight
 		mg_percentage = mg_percentvalue * 100
-		#logger.info("mg_percentage = #{mg_percentage}")
+		logger.info("mg_percentage = #{mg_percentage}")
 
 		g_gradeid = nil
 
@@ -176,12 +176,12 @@ class MetricGrade < ActiveRecord::Base
 		grades = Grade.all
 		grades.each do |grade|
 			grademin = grade.minrange
-			#logger.info("grademin = #{grademin}")
+			logger.info("grademin = #{grademin}")
 			grademax = grade.maxrange
-			#logger.info("grademax = #{grademax}")
+			logger.info("grademax = #{grademax}")
 			if mg_percentage.to_f <= grademax.to_f and mg_percentage.to_f > grademin.to_f
 				gradeval = grade.gradevalue
-				#logger.info("GRADE = #{gradeval}")
+				logger.info("GRADE = #{gradeval}")
 				g_gradeid = grade.id
 			elsif mg_percentage.to_f == 0
 				gradeval = "F"
--- db/migrate/20141010201220_create_metric_grades.rb
+++ db/migrate/20141010201220_create_metric_grades.rb
@@ -3,8 +3,8 @@ class CreateMetricGrades < ActiveRecord::Migration
     create_table :metric_grades do |t|
       t.integer :metric_id
       t.integer :grade_id
-      t.integer :points
-      t.integer :percentage
+      t.decimal :points, precision: 10, scale: 2
+      t.decimal :percentage, precision: 10, scale: 2
 
       t.timestamps
     end
